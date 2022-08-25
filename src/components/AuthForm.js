ebdf0787b73058070a6e1732a1338b06ce714927
shankar-balasubramaniam <shankarb3061997@gmail.com>
2022-08-25 21:34:14 +0530

    extract form logic to AuthForm

---
+++ src/components/AuthForm.js
@@ -0,0 +1,44 @@
+import { Button, Input, Text } from '@rneui/themed';
+import React, { useState } from 'react';
+
+import Spacer from './Spacer';
+import { StyleSheet } from 'react-native';
+
+const AuthForm = ({ headerText, errorMessage, buttonText, onSubmit }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <>
+      <Spacer>
+        <Text h3>{headerText}</Text>
+      </Spacer>
+      <Input
+        label='Email'
+        value={email}
+        onChangeText={setEmail}
+        autoCapitalize='none'
+        autoCorrect={false}
+      />
+      <Input
+        label='Password'
+        value={password}
+        onChangeText={setPassword}
+        autoCapitalize='none'
+        autoCorrect={false}
+        secureTextEntry
+      />
+      {errorMessage ? (
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
+      ) : null}
+      <Spacer />
+      <Button
+        title={buttonText}
+        onPress={() => onSubmit({ email, password })}
+      />
+    </>
+  );
+};
+
+export default AuthForm;
+
+const styles = StyleSheet.create({});
--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -3,42 +3,24 @@ import React, { useContext, useState } from 'react';
 import { StatusBar, StyleSheet, TouchableOpacity, View } from 'react-native';
 
 import { Context as AuthContext } from '../context/AuthContext';
+import AuthForm from '../components/AuthForm';
 import Spacer from '../components/Spacer';
 import { useNavigation } from '@react-navigation/native';
 
 const SignupScreen = () => {
   const navigation = useNavigation();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
 
   const { state, signup } = useContext(AuthContext);
 
   return (
     <View style={styles.container}>
       <StatusBar />
-      <Spacer>
-        <Text h3>SignUp for Tracker</Text>
-      </Spacer>
-      <Input
-        label='Email'
-        value={email}
-        onChangeText={setEmail}
-        autoCapitalize='none'
-        autoCorrect={false}
+      <AuthForm
+        headerText='SignUp for Tracker'
+        errorMessage={state.errorMessage}
+        onSubmit={signup}
+        buttonText='Sign Up'
       />
-      <Input
-        label='Password'
-        value={password}
-        onChangeText={setPassword}
-        autoCapitalize='none'
-        autoCorrect={false}
-        secureTextEntry
-      />
-      {state.errorMessage ? (
-        <Text style={styles.errorMessage}>{state.errorMessage}</Text>
-      ) : null}
-      <Spacer />
-      <Button title='SignUp' onPress={() => signup({ email, password })} />
       <Spacer>
         <TouchableOpacity onPress={() => navigation.navigate('Signin')}>
           <Text style={styles.link}>
